1d36bdde77250009b69ab49acb5a47be5ab232c6
Teresa Campbell <teresa.j.campbell@gmail.com>
2022-06-30 14:38:33 -0400

    added html, css, and beginning of js

---
+++ index.js
@@ -0,0 +1,6 @@
+var eightBallEl = document.querySelector("#magic8-img");
+var textBoxEl = document.querySelector("#text-column");
+var nameBoxEl = document.querySelector("#name-box");
+var enterButton1 = document.querySelector("#enter-button1");
+var enterButton2 = document.querySelector("#enter-button2");
+var questionBoxEl = document.querySelector("#question-box")
